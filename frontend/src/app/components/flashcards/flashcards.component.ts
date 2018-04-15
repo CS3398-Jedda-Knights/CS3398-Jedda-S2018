0e8b8080f463f0046060ac4f5bb0cd86ca0fe5ee
MAD1364 <35576888+MAD1364@users.noreply.github.com>
2018-04-15 03:11:14 -0500

    What the crap

--- frontend/src/app/components/flashcards/flashcards.component.ts
+++ frontend/src/app/components/flashcards/flashcards.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { FlashcardService } from '../../services/flashcard.service';
+import { Router, Route } from '@angular/router';
 
 @Component({
   selector: 'app-flashcards',
@@ -9,7 +11,10 @@ import { UserService } from '../../services/user.service';
 export class FlashcardsComponent implements OnInit {
   private user;
   private username;
-  constructor(private userService: UserService) { }
+  private question: string;
+  private answer: string;
+  private subject: string;
+  constructor(private userService: UserService, private flashCardService: FlashcardService, private router: Router) { }
 
   ngOnInit() {
     this.username = localStorage.getItem('current_user');
@@ -21,5 +26,23 @@ export class FlashcardsComponent implements OnInit {
       console.log(this.user);
     });
   }
+
+  onCreateFlashCard() {
+    let body = {
+      "username": this.username,
+      "subject": this.subject,
+      "question": this.question,
+      "answer": this.answer,
+    };
+    // console.log(body)
+
+    this.flashCardService.createFlashCard(body).subscribe(data => {
+      this.subject = "";
+      this.question = "";
+      this.answer = "";
+    }, error=> {
+      console.log(error);
+      this.router.navigate(['/user-profile',this.username]);
+    });
 
 }
