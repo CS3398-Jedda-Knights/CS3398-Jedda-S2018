b4a5880ab62f02a5f1c2bd99f5114a3a6a1ca1a2
MAD1364 <35576888+MAD1364@users.noreply.github.com>
2018-04-15 14:11:04 -0500

    Need to get flashcards to show (might have to do with the current view)

--- frontend/src/app/components/flashcards/flashcards.component.ts
+++ frontend/src/app/components/flashcards/flashcards.component.ts
@@ -11,6 +11,7 @@ export class FlashcardsComponent implements OnInit {
   
   private user;
   private username;
+  flashcards: string[];
 
   constructor(private userService: UserService) { }
 
@@ -20,7 +21,8 @@ export class FlashcardsComponent implements OnInit {
     this.userService.getUser(this.username).subscribe(data => {
       this.user = data['user'];
       // fake flashcard. GRAB REAL DATA WHEN API IS READY
-      this.user.flashcards = [{'question': 'Flyweight', 'answer': 'Flyweight is a software design pattern. A flyweight is an object that minimizes memory use by sharing as much data as possible with other similar objects; it is a way to use objects in large numbers when a simple repeated representation would use an unacceptable amount of memory.'}]
+      //this.user.flashcards = [{'question': 'Flyweight', 'answer': 'Flyweight is a software design pattern. A flyweight is an object that minimizes memory use by sharing as much data as possible with other similar objects; it is a way to use objects in large numbers when a simple repeated representation would use an unacceptable amount of memory.'}]
+      this.flashcards = this.user.flashcards;
       console.log(this.user);
     });
   }
