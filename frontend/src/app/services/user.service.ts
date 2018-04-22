5e41f3d710f9699e78c2cef52b1d74cd5b728f7c
Kentessa F. Goldstein <k_f145@txstate.edu>
2018-04-22 16:31:36 -0500

    add services for editing user profile

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -34,5 +34,44 @@ export class UserService {
       .catch((error: any) => error.json() || 'Server error');
   }
 
+  // Untested 
+  updateUserFullName(username, body) {
+    this.url = APP_SERVER + 'user/update_fn' + username;
+    this.authToken = localStorage.getItem('access_token');
+    this.options = new RequestOptions({ headers: new Headers({ 'Content-Type': 'application/json', 'Authorization': `JWT ${this.authToken}` }) });
 
+    return this.http.post(this.url,this.options, body)
+      .map((res: Response) => res.json())
+      .catch((error: any) => error.json() || 'Server error');
+  }
+
+  updateUserStatus(username, body) {
+    this.url = APP_SERVER + 'user/update_status' + username;
+    this.authToken = localStorage.getItem('access_token');
+    this.options = new RequestOptions({ headers: new Headers({ 'Content-Type': 'application/json', 'Authorization': `JWT ${this.authToken}` }) });
+
+    return this.http.post(this.url, this.options, body)
+      .map((res: Response) => res.json())
+      .catch((error: any) => error.json() || 'Server error');
+  }
+
+  updateUserBio(username, body) {
+    this.url = APP_SERVER + 'user/update_bio' + username;
+    this.authToken = localStorage.getItem('access_token');
+    this.options = new RequestOptions({ headers: new Headers({ 'Content-Type': 'application/json', 'Authorization': `JWT ${this.authToken}` }) });
+
+    return this.http.post(this.url, this.options, body)
+      .map((res: Response) => res.json())
+      .catch((error: any) => error.json() || 'Server error');
+  }
+
+  deactivateUser(username) {
+    this.url = APP_SERVER + 'user/deactivate' + username;
+    this.authToken = localStorage.getItem('access_token');
+    this.options = new RequestOptions({ headers: new Headers({ 'Content-Type': 'application/json', 'Authorization': `JWT ${this.authToken}` }) });
+
+    return this.http.delete(this.url, this.options)
+      .map((res: Response) => res.json())
+      .catch((error: any) => error.json() || 'Server error');
+  }
 }
