fdc1409238d6c0c96efba3422b9f87e5c3744ed9
jh2012 <jh2012@txstate.edu>
2018-04-22 22:02:54 -0500

    Reorganized Note VIew and allowed for Update Note

--- frontend/src/app/components/flashcards/flashcards.component.ts
+++ frontend/src/app/components/flashcards/flashcards.component.ts
@@ -12,6 +12,8 @@ export class FlashcardsComponent implements OnInit {
   private user;
   private username;
   flashcards: string[];
+  decks: string[] = [];
+  currentDeck: string[] = [];
 
   constructor(private userService: UserService) { }
 
@@ -23,6 +25,15 @@ export class FlashcardsComponent implements OnInit {
       // fake flashcard. GRAB REAL DATA WHEN API IS READY
       //this.user.flashcards = [{'question': 'Flyweight', 'answer': 'Flyweight is a software design pattern. A flyweight is an object that minimizes memory use by sharing as much data as possible with other similar objects; it is a way to use objects in large numbers when a simple repeated representation would use an unacceptable amount of memory.'}]
       this.flashcards = this.user.flashcards;
+      localStorage.setItem('Current_Deck',this.flashcards[0]['subject']);
+      for(let flashcard of this.flashcards){
+        if(flashcard['subject'] == localStorage.getItem('Current_Deck')){
+          this.currentDeck.push(flashcard);
+        }
+        if (!this.decks.includes(flashcard['subject'])){
+          this.decks.push(flashcard['subject']);
+        }
+      }
       console.log(this.user);
     });
   }
--- frontend/src/app/components/notebook/notebook.component.ts
+++ frontend/src/app/components/notebook/notebook.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../../services/user.service';
+import { NoteService } from '../../services/note.service';
 
 @Component({
   selector: 'app-notebook',
@@ -15,9 +16,17 @@ export class NotebookComponent implements OnInit {
   sectionsContent:string[][];
   noteViews:string[];
   notes:string[];
-  str:string = "Hello World!";
+  currentNotes: string[] = [];
+  subjects: string[] = [];
+  note: string;
+  title: string;
+  currentHTML: string;
+  editNoteTitleHTMLbeggining: string = "<div class=\"margin-bottom:20px\">Notes<textarea rows=\"1\" id=\"note-title\" class=\"form-control\" name=\"title\" [(ngModel)]=\"title\" required>";
+  editNoteBodyHTMLbeggining: string = "<div class=\"margin-bottom:20px\">Notes<textarea rows=\"10\" id=\"note-body\" class=\"form-control\" name=\"note\" [(ngModel)]=\"note\" required>";
+  editNoteTitleHTMLend: string = "</textarea></div>";
+  editNoteBodyHTMLend: string = "</textarea></div>";
 
-  constructor(private route: ActivatedRoute, private userService: UserService) {}
+  constructor(private route: ActivatedRoute, private userService: UserService, private noteService: NoteService) {}
 
   ngOnInit() {
     this.username = localStorage.getItem('current_user');
@@ -26,7 +35,69 @@ export class NotebookComponent implements OnInit {
       this.user = data['user'];
       // fake flashcard. GRAB REAL DATA WHEN API IS READY
       this.notes = this.user.notes;
+      localStorage.setItem('Current_Subject',this.notes[0]['subject']);
+      for(let note of this.notes){
+        if(note['subject'] == localStorage.getItem('Current_Subject')){
+          this.currentNotes.push(note);
+        }
+        if (!this.subjects.includes(note['subject'])){
+          this.subjects.push(note['subject']);
+        }
+      }
       console.log(this.user);
+    });
+  }
+  
+  viewNoteBook(subject:string){
+    this.currentNotes = []
+    localStorage.setItem('Current_Subject',subject);
+    for(let note of this.notes){
+      if(note['subject'] == subject){
+        this.currentNotes.push(note);
+      }
+    }
+  }
+
+  noteEditor(note:JSON){
+    console.log(document.getElementById("title" + note['title']).textContent);
+    document.getElementById("title" + note['title']).style.border = "1px solid black";
+    document.getElementById("body" + note['title']).style.border = "1px solid black";
+    document.getElementById("title" + note['title']).contentEditable = "true";
+    document.getElementById("body" + note['title']).contentEditable = "true";
+    document.getElementById("button" + note['title']).style.visibility = "visible";
+    document.getElementById("edit" + note['title']).style.visibility = "hidden";
+    document.getElementById("trash" + note['title']).style.visibility = "hidden";
+  }
+
+  editNote(note:JSON){
+    //document.getElementById(this.currentNotes[this.currentNote]['title']).innerHTML = this.currentNotes[this.currentNote]['body'];
+    for(var i = 0; i < this.currentNotes.length; i++){
+      if(this.currentNotes[i]['id'] == note['id']){
+        this.currentNotes[i]['title'] = document.getElementById("title" + note['title']).textContent;
+        this.currentNotes[i]['body'] = document.getElementById("body" + note['title']).textContent;
+      }
+    }
+
+    let body = {
+      "username": localStorage.getItem('current_user'),
+      "title": document.getElementById("title" + note['title']).textContent,
+      "subject": note['subject'],
+      "body": document.getElementById("body" + note['title']).textContent,
+      "active": 'T',
+    };
+    // console.log(body)
+
+    this.noteService.updateNote(body,note['id']).subscribe(data => {
+      document.getElementById("title" + note['title']).style.border = "none";
+      document.getElementById("body" + note['title']).style.border = "none";
+      document.getElementById("title" + note['title']).contentEditable = "false";
+      document.getElementById("body" + note['title']).contentEditable = "false";
+      document.getElementById("button" + note['title']).style.visibility = "hidden";
+      document.getElementById("edit" + note['title']).style.visibility = "visible";
+      document.getElementById("trash" + note['title']).style.visibility = "visible";
+
+    }, error=> {
+      console.log(error);
     });
   }
   //   //this.sections = ['Electronics','Digital Signal Processing','History'];
